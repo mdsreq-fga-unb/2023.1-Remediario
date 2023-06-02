a3f7f029c07528e0b605e130a58cde1120f4edd7
Mateuszinnn <mateusdealmeidadias@gmail.com>
2023-06-02 12:24:11 -0300

    Pagina para adicionar remedio

--- src/pages/AddRemedio/index.jsx
+++ src/pages/AddRemedio/index.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
+import { styles } from './styles';
+import { TextInput } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-export default function AddRemedio () {
+export default function AddRemedio() {
   const [remedio, setRemedio] = useState('');
   const [dosagem, setDosagem] = useState('');
   const [quantidade, setQuantidade] = useState(1);
   const [unidade, setUnidade] = useState('comprimidos');
+  const [intervalo, setIntervalo] = useState(1);
+  const [unidadeIntervalo, setUnidadeIntervalo] = useState('horas');
 
   return (
     <View style={styles.container}>
@@ -16,35 +21,65 @@ export default function AddRemedio () {
         value={remedio}
         onChangeText={setRemedio}
         placeholder="Digite o nome do remédio"
+        underlineColor="white"
+        placeholderTextColor="white"
       />
 
       <Text style={styles.label}>Dosagem</Text>
-      <View style={styles.dosagemContainer}>
-        <Picker
-          style={styles.dropdown}
-          selectedValue={quantidade}
-          onValueChange={(itemValue) => setQuantidade(itemValue)}
-        >
-          {Array.from({ length: 1000 }, (_, index) => (
-            <Picker.Item key={index} label={`${index + 1}`} value={index + 1} />
-          ))}
-        </Picker>
+      <View style={styles.ContainerDropdown}>
+        <View style={styles.dropdown}>
+          <Picker
+            style={styles.dropdownPicker}
+            selectedValue={quantidade}
+            onValueChange={(itemValue) => setQuantidade(itemValue)}
+          >
+            {Array.from({ length: 1000 }, (_, index) => (
+              <Picker.Item key={index} label={`${index + 1}`} value={index + 1} />
+            ))}
+            <Icon name={i % 2 === 0 ? 'check' : 'close'} style={styles.optionIcon} />
+          </Picker>
+        </View>
+        <View style={styles.dropdown}>
+          <Picker
+            style={styles.dropdownPicker}
+            selectedValue={unidade}
+            onValueChange={(itemValue) => setUnidade(itemValue)}
+          >
+            <Picker.Item label="comprimidos" value="comprimidos" />
+            <Picker.Item label="ml" value="ml" />
+            <Picker.Item label="mg" value="mg" />
+          </Picker>
+        </View>
 
-        <Picker
-          style={styles.dropdown}
-          selectedValue={unidade}
-          onValueChange={(itemValue) => setUnidade(itemValue)}
-        >
-          <Picker.Item label="comprimidos" value="comprimidos" />
-          <Picker.Item label="ml" value="ml" />
-          <Picker.Item label="mg" value="mg" />
-        </Picker>
+        <Text style={styles.secondarylabel}>por dosagem</Text>
+      </View>
 
-        <Text style={styles.dosagemText}>por dosagem</Text>
+      <View style={styles.ContainerDropdown}>
+        <Text style={styles.secondarylabel}>A cada</Text>
+        <View style={styles.dropdown}>
+          <Picker
+            style={styles.dropdownPicker}
+            selectedValue={intervalo}
+            onValueChange={(itemValue) => setIntervalo(itemValue)}
+          >
+            {Array.from({ length: 10 }, (_, index) => (
+              <Picker.Item key={index} label={`${index + 1}`} value={index + 1} />
+            ))}
+          </Picker>
+        </View>
+        <View style={styles.dropdown}>
+          <Picker
+            style={styles.dropdownPicker}
+            selectedValue={unidadeIntervalo}
+            onValueChange={(itemValue) => setUnidadeIntervalo(itemValue)}
+          >
+            <Picker.Item label="meses" value="meses" />
+            <Picker.Item label="dias" value="dias" />
+            <Picker.Item label="horas" value="horas" />
+            <Picker.Item label="minutos" value="minutos" />
+          </Picker>
+        </View>
       </View>
     </View>
   );
-};
-
-
-
+}
--- src/pages/AddRemedio/styles.jsx
+++ src/pages/AddRemedio/styles.jsx
@@ -1,50 +1,55 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
+import { Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: "center",
-    },
-
-    text: {
-        color: '#000',
-        fontSize: 30,
-        fontWeight: 'bold',
-
-    }
-    
-        container: {
-          flex: 1,
-          backgroundColor: '#1E1E1E',
-          padding: 20,
-        },
-        label: {
-          fontSize: 18,
-          color: 'white',
-          marginBottom: 5,
-        },
-        input: {
-          backgroundColor: 'white',
-          borderRadius: 8,
-          padding: 10,
-          marginBottom: 20,
-        },
-        dosagemContainer: {
-          flexDirection: 'row',
-          alignItems: 'center',
-        },
-        dropdown: {
-          flex: 1,
-          height: 40,
-          backgroundColor: 'white',
-          borderRadius: 8,
-          marginRight: 10,
-        },
-        dosagemText: {
-          color: 'white',
-          marginLeft: 10,
-        },
-      
+  container: {
+    flex: 1,
+    backgroundColor: '#161B1B',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  label: {
+    fontSize: 18,
+    color: 'white',
+    marginBottom: 10,
+    marginTop: 10,
+    marginLeft: 10,
+    alignSelf: 'flex-start',
+  },
+  secondarylabel: {
+    color: 'white',
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  input: {
+    backgroundColor: '#161B1B',
+    borderColor: 'white',
+    borderWidth: 2,
+    borderRadius: 8,
+    width: width - 20,
+    color: 'white',
+  },
+  ContainerDropdown: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  dropdown: {
+    flex: 1,
+    backgroundColor: '#161B1B',
+    borderRadius: 8,
+    borderColor: 'white',
+    borderWidth: 2,
+    marginLeft: 5,
+    marginRight: 5,
+  },
+  dropdownPicker: {
+    color: 'white',
+  },
+  optionIcon: {
+    fontSize: 10,
+    color: 'white',
+  },
 });
