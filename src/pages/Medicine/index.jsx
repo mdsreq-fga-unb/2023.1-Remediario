d37141267793add2aa4fa7de3ec5888bc6f0025a
Julio Cesar <juliodourado419@gmail.com>
2023-06-03 17:02:53 -0300

    resolving conflits

--- src/pages/Medicine/index.jsx
+++ src/pages/Medicine/index.jsx
@@ -6,13 +6,9 @@ import RemedioDropdown from '../../Components/Remedio';
 export default function Medicine() {
     return (
         <View style={styles.container}>
-<<<<<<< HEAD
-            <Text style={styles.text}>Meus Remedios</Text>
-=======
             <Text style={styles.text}>TODOS OS MEDICAMENTOS</Text>
             <RemedioDropdown nomeRemedio="Nome do Remedio" />
->>>>>>> origin/dev
         </View>
-        
+
     )
 }
