0f39d00c930ddde24caf25786e98d9a81038cdb5
Luan Melo <luanmelo.q@gmail.com>
2023-06-07 17:25:03 -0300

    tela de confirmacao

--- src/pages/ConfirmationMedicine/index.jsx
+++ src/pages/ConfirmationMedicine/index.jsx
@@ -1,11 +1,71 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { View, Text, TouchableOpacity } from "react-native";
 import { styles } from './styles';
+import MedicineUse from "../../Components/MedicineUse";
 
-export default function Home() {
+export default function ConfirmationMedicine ({ navigation }) {
+    let hours = new Date().getHours(); //To get the Current Hours
+    let min = new Date().getMinutes(); //To get the Current Minutes
+    let quantidade = 3;
+
+    const [componentes, setComponentes] = useState(
+        Array.from({ length: quantidade }, (_, index) => ({
+          key: index,
+          variante: "medicineNotUsed",
+        }))
+    );
+
+    const [indiceAtual, setIndiceAtual] = useState(0);
+
+    const mudarVariante = () => {
+        setComponentes((prevState) => {
+            const updatedComponentes = [...prevState];
+            const componenteAtual = updatedComponentes[indiceAtual];
+            componenteAtual.variante = componenteAtual.variante === 'medicineNotUsed' ? "medicineUsed" : 'medicineUsed';
+            
+            if (indiceAtual === quantidade - 1) {
+                updatedComponentes.forEach((componente) => {
+                    componente.variante = "medicineNotUsed";
+                });
+            }
+            
+            return updatedComponentes;
+        });
+
+    setIndiceAtual((prevIndice) => (prevIndice + 1) % quantidade);
+    
+    setTimeout(() => {
+        navigation.navigate('Home');
+      }, 1000);
+  };
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>Confirmacao</Text>
+            <View style={styles.header}>
+                <Text style={styles.horario}>{hours}:{min}</Text>
+                <Text style={styles.remedio}>Dipirona</Text>
+            </View>
+            <View style={styles.buttons}>
+                <TouchableOpacity style={styles.confirmButton} onPress={mudarVariante}>
+                    <Icon name="check" color="#FFF" size={40}/>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.delayButton}>
+                    <Text style={styles.textDelayButton}>+5</Text>
+                </TouchableOpacity>
+            </View>
+            <View style={styles.medicineUseContainer}>
+                {componentes.map((componente, index) => (
+                    <MedicineUse
+                    key={componente.key}
+                    variante={componente.variante}
+                    onPress={() => mudarVariante(index)}
+                    />
+                ))}
+            </View>
         </View>
     );
-}
+};
+
+
+
