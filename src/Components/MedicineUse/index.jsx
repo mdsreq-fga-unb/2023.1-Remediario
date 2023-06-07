5879cb13f58baf2adda49a69693c3705ac5db3ff
Luan Melo <luanmelo.q@gmail.com>
2023-06-07 17:24:54 -0300

    componente de uso de medicamento

---
+++ src/Components/MedicineUse/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { View } from 'react-native'
+import { styles } from './styles';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+export default function MedicineUse({variante}) {
+  return (
+        <View>
+            {variante == 'medicineUsed' && 
+                <View style={styles.medicineUsed}>
+                    <Icon name="check" color="#FFF" size={24}/>
+                </View>
+            }
+            {variante == 'medicineNotUsed' &&
+                <View style={styles.medicineNotUsed}>
+                    <Icon name="check" color="#2E2E2E" size={24}/>
+                </View>
+            }
+        </View>
+        
+    )
+}
---
+++ src/Components/MedicineUse/styles.jsx
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    medicineUsed: {
+        width: 40,
+        height: 40,
+        borderWidth: 1,
+        borderColor: '#FFF',
+        borderRadius: 100,
+
+        backgroundColor: '#006B65',
+        alignItems: 'center',
+        justifyContent: "center",
+    },
+    medicineNotUsed: {
+        width: 40,
+        height: 40,
+        borderWidth: 1,
+        borderColor: '#FFF',
+        borderRadius: 100,
+
+        backgroundColor: '#161B1B',
+        alignItems: 'center',
+        justifyContent: "center",
+    },
+});
