01685ce0be875ecd665fd8ba73e6bc5c0d133b4e
Luan Melo <luanmelo.q@gmail.com>
2023-06-14 20:39:24 -0300

    deep link por notificacao

--- App.jsx
+++ App.jsx
@@ -1,17 +1,7 @@
-import { createURL, useURL } from 'expo-linking'
-import { Text } from 'react-native';
 import Routes from './src/routes';
 
 export default function App() {
-  const redirectURL = useURL();
-
-  const url = createURL('confirmation', {});
-  console.log(url);
-
   return (
-    <>
-      <Text>{redirectURL}</Text>
-      <Routes />
-    </>
+    <Routes />
   );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { NavigationContainer, useIsFocused } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity, Linking, Text } from 'react-native';
 import Home from '../pages/Home';
 import History from '../pages/History';
 import Medicine from '../pages/Medicine';
 import { createStackNavigator } from '@react-navigation/stack';
-import Confirmation from '../pages/ConfirmationMedicine';
+import Confirmacao from '../pages/ConfirmationMedicine';
 import About from '../pages/About';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome5Icon from 'react-native-vector-icons/FontAwesome5';
 import AddMedicine from '../pages/AddRemedio/index';
+import * as Notifications from 'expo-notifications';
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+});
 
 function TabRoutes() {
     const sizeIcons = 24; // Define o tamanho dos ícones como 24px
@@ -74,13 +83,83 @@ const styles = StyleSheet.create({
     },
 });
 
+let medicineName = 'Dipirona';
+let medicineTimer = 2;
+
+async function schedulePushNotification() {
+    const { status } = await Notifications.getPermissionsAsync();
+
+    if (status !== 'granted') {
+      Alert.alert('Você não deixou as notificações ativas');
+      return;
+    }else {
+        await Notifications.scheduleNotificationAsync({
+            content: {
+              title: medicineName,
+              subtitle: 'Está na hora de tomar o remédio',
+              data: {
+                url: 'exp://192.168.42.152:19000//--/remediario/Confirmacao'
+              }
+            },
+            trigger: {
+              seconds: medicineTimer,
+            },
+        });
+    }
+};
+
 export default function Routes() {
     return (
-        <NavigationContainer>
+        <NavigationContainer linking={{
+            prefixes: ['exp://192.168.42.152:19000/--/remediario'],
+            config: {
+                screens: {
+                    Confirmacao: {
+                        path: 'Confirmacao'
+                    }
+                }
+            },
+
+            async getInitialURL() {
+                // First, you may want to do the default deep link handling
+                // Check if app was opened from a deep link
+                const url = await Linking.getInitialURL();
+
+                if (url != null) {
+                  return url;
+                }
+      
+                // Handle URL from expo push notifications
+                const response = await Notifications.getLastNotificationResponseAsync();
+
+                return response?.notification.request.content.data.url;
+              },
+            subscribe(listener) {
+                const onReceiveURL = ({url}) => listener(url);
+
+                // Listen to incoming links from deep linking
+                const eventListenerSubscription = Linking.addEventListener('url', onReceiveURL);
+
+                // Listen to expo push notifications
+                const subscription = Notifications.addNotificationResponseReceivedListener(response => {
+                const url = response.notification.request.content.data.url;
+                    listener(url);
+                });
+
+                return () => {
+                    // Clean up the event listeners
+                    eventListenerSubscription.remove();
+                    subscription.remove();
+                };
+            },
+        }}>
             <Stack.Navigator>
                 <Stack.Screen options={{ headerShown: false }} name="BottomTab" component={TabRoutes}/>
-                <Stack.Screen options={{ headerShown: false }} name='Confirmacao' component={Confirmation}/>
+                <Stack.Screen options={{ headerShown: false }} name='Confirmacao' component={Confirmacao}/>
             </Stack.Navigator>
+            <TouchableOpacity style={styles.button} onPress={async () => {await schedulePushNotification();}}>
+                <Text>Enviar notificação</Text>
+            </TouchableOpacity>
         </NavigationContainer>
     );
 }
