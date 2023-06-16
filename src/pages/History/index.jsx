1a02b4aa148e908ff2fbbbed2266ff8a88d30124
Luan Melo <luanmelo.q@gmail.com>
2023-06-16 17:38:22 -0300

    tela de historico

--- src/pages/History/index.jsx
+++ src/pages/History/index.jsx
@@ -1,10 +1,16 @@
 import { View, Text } from "react-native";
 import { styles } from './styles';
+import RemedioDropdown from '../../Components/Remedio';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function History() {
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>HISTORICO DE MEDICAMENTOS</Text>
+            <View>
+                <Text>Junho</Text>
+                <Icon name='download' color='white' size='24' />
+            </View>
+            <RemedioDropdown nomeRemedio="Nome do Remedio" />
         </View>
     )
 }
