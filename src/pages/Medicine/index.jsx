e8e39bce79f5b0ce2250890a1427263a7c25f130
Luan Melo <luanmelo.q@gmail.com>
2023-06-16 17:38:06 -0300

    tirando dropdown da tela de medicamentos

--- src/pages/Medicine/index.jsx
+++ src/pages/Medicine/index.jsx
@@ -1,13 +1,10 @@
 import { View, Text } from "react-native";
 import { styles } from './styles';
-import RemedioDropdown from '../../Components/Remedio';
-
 
 export default function Medicine() {
     return (
         <View style={styles.container}>
             <Text style={styles.text}>TODOS OS MEDICAMENTOS</Text>
-            <RemedioDropdown nomeRemedio="Nome do Remedio" />
         </View>
 
     )
