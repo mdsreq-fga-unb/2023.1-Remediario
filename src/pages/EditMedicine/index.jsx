ad09441f72b3f14375f540c8e67c10140504422f
Mateuszinnn <mateusdealmeidadias@gmail.com>
2023-06-28 14:55:17 -0300

    Update EditMedicine

--- src/pages/EditMedicine/index.jsx
+++ src/pages/EditMedicine/index.jsx
@@ -1,26 +1,44 @@
-import { Text } from "react-native";
-import { View } from 'react-native';
-import { styles } from './styles';
-import AddRemedio from '../../Components/RemedioForm/index';
-import RemedioForm from '../../Components/RemedioForm';
-import React, { useState } from 'react';
-import { SalvarMedicamento } from '../../Services/medicamento';
+import React, { useState, useEffect } from 'react';
+import { View, Text } from 'react-native';
+import RemedioForm from '../../Components/RemedioForm/index';
+import { SalvarMedicamento, getMedicamento } from '../../Services/medicamento';
 
 export default function EditMedicine({ navigation }) {
-    const handleSave = async (remedio) => {
-        try {
-          await SalvarMedicamento(remedio);
-          console.log('Medicamento Salvo');
-          setTimeout(() => {
-            navigation.navigate('Remédios do dia');
-          }, 1000);
-        } catch (e) {
-          console.log(e);
-        }
-      };
-    return (
-        <View style={styles.container}>
-        <RemedioForm onSave={handleSave} navigation={navigation} />
-      </View>
-    );
+  const [remedioData, setRemedioData] = useState(null);
+
+  useEffect(() => {
+    const fetchRemedioData = async () => {
+      try {
+        const nomeRemedio = 'Dipirona'; // Substitua pelo nome do remédio desejado
+        const remedio = await getMedicamento(nomeRemedio);
+        setRemedioData(remedio);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchRemedioData();
+  }, []);
+
+  const handleSave = async (remedio) => {
+    try {
+      await SalvarMedicamento(remedio);
+      console.log('Medicamento Salvo');
+      setTimeout(() => {
+        navigation.navigate('Remédios do dia');
+      }, 1000);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <View>
+      {remedioData ? (
+        <RemedioForm remedio={remedioData} onSave={handleSave} navigation={navigation} />
+      ) : (
+        <Text>Carregando dados do remédio...</Text>
+      )}
+    </View>
+  );
 }
