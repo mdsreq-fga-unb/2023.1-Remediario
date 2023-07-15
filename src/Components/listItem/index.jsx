4554a6b582e3d322bbda11cc1fe888520d52e450
Julio Cesar <juliodourado419@gmail.com>
2023-07-14 22:30:18 -0300

    resolving bug :)

--- src/Components/listItem/index.jsx
+++ src/Components/listItem/index.jsx
@@ -1,4 +1,4 @@
-
+import React, { useEffect, useState, useCallback } from 'react';
 import { View, Text, TouchableOpacity, Alert } from 'react-native';
 import { styles } from './styles';
 import Icon from "react-native-vector-icons/MaterialIcons";
@@ -7,32 +7,37 @@ import { ProgressBar } from 'react-native-paper';
 import Timer from '../Timer';
 
 export default function ListItem({ remedio, atualizarLista, navigation }) {
+  const [horaTimer, setHoraTimer] = useState(0);
+  const [minutoTimer, setMinutoTimer] = useState(0);
+  const [segundoTimer, setSegundoTimer] = useState(0);
 
+  const dados = new Date(remedio.ultimoAlarme);
+  const horasAlarme = dados.getHours();
+  const minutosAlarme = dados.getMinutes();
+  const nome = remedio.nomeRemedio;
+  const estoque = remedio.estoque;
+  const progresso = (estoque / remedio.dosagem) / 10;
 
+  useEffect(() => {
+    const atualizarTimer = () => {
+      const agora = new Date();
+      const tempoMs = dados.getTime() - agora.getTime();
 
-  //informações do remedio
-  let dados = new Date(remedio.ultimoAlarme);
-  let horasAlarme = dados.getHours();
-  let minutos = dados.getMinutes();
-  let nome = remedio.nomeRemedio;
-  let estoque = remedio.estoque;
-  let progress = (estoque / remedio.dosagem) / 10;
+      let segundos = Math.floor(tempoMs / 1000);
+      let horas = Math.floor(segundos / 3600);
+      segundos = segundos % 3600;
+      let minutos = Math.floor(segundos / 60);
+      segundos = segundos % 60;
 
-  //informações do celular
-  let now = new Date();
+      setHoraTimer(horas);
+      setMinutoTimer(minutos);
+      setSegundoTimer(segundos);
+    };
 
-  let tempoMs = dados.getTime() - now.getTime();
+    const timerId = setTimeout(atualizarTimer, 1);
 
-  // Converter para segundos
-  let segundosTimer = Math.floor(tempoMs / 1000);
-
-  // Calcular horas
-  let horasTimer = Math.floor(segundosTimer / 3600);
-  segundosTimer = segundosTimer % 3600;
-
-  // Calcular minutos
-  let minutosTimer = Math.floor(segundosTimer / 60);
-  segundosTimer = segundosTimer % 60;
+    return () => clearTimeout(timerId);
+  }, [remedio.ultimoAlarme]);
 
   const confirmarRemocao = () => {
     Alert.alert(
@@ -52,66 +57,69 @@ export default function ListItem({ remedio, atualizarLista, navigation }) {
     );
   };
 
-  async function remover() {
+  const remover = async () => {
     try {
       await RemoverMedicamento(nome);
       atualizarLista();
     } catch (e) {
       console.log(e);
     }
-  }
+  };
 
-  function redirect() {
-    if (estoque == 0) {
-      Alert.alert("É preciso repor o estoque para usar o medicamento")
+  const redirect = useCallback(() => {
+    if (estoque === 0) {
+      Alert.alert("É preciso repor o estoque para usar o medicamento");
     } else {
+      const agora = new Date();
+      const tempoMs = dados.getTime() - agora.getTime();
+      let segundos = Math.floor(tempoMs / 1000);
+      let horas = Math.floor(segundos / 3600);
+      segundos = segundos % 3600;
+      let minutos = Math.floor(segundos / 60);
+      segundos = segundos % 60;
+
+      setHoraTimer(horas);
+      setMinutoTimer(minutos);
+      setSegundoTimer(segundos);
+
       navigation.navigate('Confirmacao', {
         medicineName: nome,
       });
     }
-  }
+  }, [estoque, nome, navigation, dados]);
 
-  function editMedicine() {
-    // Função para editar o medicamento
+  const editarMedicamento = () => {
     navigation.navigate('EditMedicine', {
       medicineName: nome,
     });
   };
-
-  if (minutos < 10) {
-    minutos = "0" + minutos;
-  }
 
   return (
     <TouchableOpacity style={styles.container} onPress={redirect}>
       <View style={styles.container2}>
         <Text style={styles.text}>{nome}</Text>
         <View style={styles.alignEnd}>
-
           <View style={styles.contentHours}>
             <Text style={styles.text2}>
-              <Timer horaTimer={horasTimer} minutoTimer={minutosTimer} segundosTimer={segundosTimer} />
+              <Timer horaTimer={horaTimer} minutoTimer={minutoTimer} segundoTimer={segundoTimer} />
             </Text>
           </View>
 
           <View style={styles.contentHours}>
             <Icon name='alarm' color={'white'} style={styles.miniIcon} />
-            <Text style={styles.text2}>{horasAlarme}:{minutos}</Text>
+            <Text style={styles.text2}>{horasAlarme}:{minutosAlarme}</Text>
           </View>
-          <TouchableOpacity style={styles.botao} onPress={editMedicine}>
+          <TouchableOpacity style={styles.botao} onPress={editarMedicamento}>
             <Icon name='edit' color={'white'} style={styles.icon} />
           </TouchableOpacity>
-
-
           <TouchableOpacity style={styles.botao} onPress={confirmarRemocao}>
             <Icon name='delete' color={'white'} style={styles.icon} />
           </TouchableOpacity>
-
         </View>
       </View>
 
       <ProgressBar
-        progress={progress}
+        progress={progresso}
         color='#006B65'
         style={styles.progressBar}
       />
