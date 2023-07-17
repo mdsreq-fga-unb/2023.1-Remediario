dfa9fc7c0ff139d3a5a8d27c9a39227fe81fa61e
Luan Melo <luanmelo.q@gmail.com>
2023-07-17 19:13:36 -0300

    download de relatorio de uso de medicamento

--- src/pages/History/styles.jsx
+++ src/pages/History/styles.jsx
@@ -7,11 +7,23 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingVertical: 20,
-        paddingHorizontal: 10,
+        paddingHorizontal: 20,
+        alignItems: 'center',
+        justifyContent: "flex-start",
+        gap: 10,
+        backgroundColor: '#161B1B',
+    },
+    remedioContainer: {
         alignItems: 'flex-start',
         justifyContent: "flex-start",
         gap: 10,
         backgroundColor: '#161B1B',
+    },
+    buttonContainer: {
+        display: 'flex',
+        alignItems: 'center',
+        backgroundColor: '#161B1B',
+        justifyContent: "center",
     },
     titleLarge: {
         color: 'white',
@@ -30,7 +42,25 @@ export const styles = StyleSheet.create({
     medicineContainer: {
         gap: 8,
     },
-    title: {
-
+    button: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#006B65',
+        paddingHorizontal: 16,
+        paddingVertical: 10,
+        borderRadius: 200,
+        width: 200,
+        marginBottom: 10,
+        marginTop: 10,
+        gap: 10,
+    },
+    text: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    icon: {
+        fontSize: 20,
     }
 });
